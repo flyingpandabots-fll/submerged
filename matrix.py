9b9eb16c1ac22736796afcebd42583562cfed7f8
krisved35 <vedika.krishnan2017@gmail.com>
2026-03-22 14:27:50 -0700

    Init changes (#1)

--- matrix.py
+++ matrix.py
@@ -7,7 +7,7 @@ from pybricks.robotics import DriveBase
 hub = PrimeHub()
 drivebase = DriveBase(Motor(Port.A, Direction.COUNTERCLOCKWISE), Motor(Port.E,Direction.CLOCKWISE), wheel_diameter=56, axle_track=114)
 drivebase.use_gyro(True)
-# Order: straight_speed, straight_acceleration, turn_rate, turn_acceleration
+# Order: straight_speed, straight_acceleration, Turn_rate, turn_acceleration
 drivebase.settings(977, 400, 977, 100)
 drivebase.straight(400, wait=True)
 drivebase.arc(-160,angle=90, wait=True)
