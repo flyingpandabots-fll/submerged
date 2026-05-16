bfe97ae47792183c204cf432eba0ac6bab390556
Shrewood17-robotics <shreyan.chundi@gmail.com>
2026-05-16 13:20:59 -0700

    adding ship and half of coral mission (#5)

---
+++ ship.py
@@ -0,0 +1,25 @@
+from pybricks.robotics import DriveBase
+from pybricks.pupdevices import Motor
+from pybricks.parameters import Icon, Port, Direction, Stop
+from pybricks.tools import wait
+from pybricks.hubs import PrimeHub
+Hub = PrimeHub()
+# Set up 
+left_motor = Motor(Port.A, Direction.COUNTERCLOCKWISE)
+right_motor = Motor(Port.E, Direction.CLOCKWISE)
+drive_base = DriveBase(left_motor, right_motor, 55,115)
+drive_base.use_gyro(True)
+
+# speed (max speed : 900)
+drive_base.settings(700, 700, 700,700) 
+
+# Move forward 40 cm (400 mm)
+drive_base.straight(-150)
+drive_base.arc(-300, -91)
+
+# Move backward 40 cm (400 mm)
+drive_base.settings(300, 300, 300, 300)
+drive_base.straight(-260)
+drive_base.straight(320)
+drive_base.arc(-300,120)
+
